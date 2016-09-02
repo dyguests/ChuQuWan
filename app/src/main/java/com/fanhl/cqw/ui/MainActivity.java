69a04e96375f20dd9d7716c1d41177ec5d57f19c
fanhl <dyguests@qq.com>
2016-09-02 11:43:00 +0800

    new main

--- app/src/main/java/com/fanhl/cqw/ui/MainActivity.java
+++ app/src/main/java/com/fanhl/cqw/ui/MainActivity.java
@@ -1,26 +1,24 @@
 package com.fanhl.cqw.ui;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import com.fanhl.cqw.R;
+import com.fanhl.cqw.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
